0345a0ead193e3552bbab59a428269f8871e402d
Thiago Bernegossi <thiago.bernegossi@outlook.com.br>
2025-05-30 18:26:46 -0300

    Módulo Intermediário · Capítulo 08 · Avaliação de Conhecimentos

---
+++ capitulo-08/knowledge_assessment.py
@@ -0,0 +1,23 @@
+# ---------------------------------------------------------------------------------------------------------------------
+# ARQUIVO: knowledge_assessment.py
+# DATA DE CRIAÇÃO: 30-05-2025
+# DATA DE PUBLICAÇÃO: 30-05-2025
+# CONTEUDISTA: Faculdade de Tecnologia (FATEC) do Estado de São Paulo
+# CONTEÚDO OFERTADO: Curso de Python – Módulo Intermediário
+# DOCENTE AUTOR: Sérgio Luiz Banin
+# DISCENTE: Thiago Bernegossi
+# DESCRIÇÃO: Arquivo que contém código-fonte desenvolvido na linguagem de programação Python para fins de aprendizado.
+# ---------------------------------------------------------------------------------------------------------------------
+
+# Questão 01-) Analise o trecho de código que segue:
+# for d in str(s):
+#     match d:
+#         case 3: print('x', end='')
+#         case 2: print('a', end='')
+#         case 1: print('y', end='')
+#         case _: print('z', end='')
+# Se s = 123, o que seria impresso por este trecho de código?
+# Resposta: c. zzz
+
+# Questão 02-) Deseja-se utilizar um comando if de uma única linha para incrementar uma variável x de uma unidade apenas quando seu valor é impar. Qual das alternativas que seguem está INCORRETA?
+# Resposta: d. x = x if x % 2 == 0 else x = x + 1
