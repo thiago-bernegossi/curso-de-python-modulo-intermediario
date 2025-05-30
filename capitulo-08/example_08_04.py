eedccebb0d57d871686a63895559b837262911c3
Thiago Bernegossi <thiago.bernegossi@outlook.com.br>
2025-05-30 17:59:46 -0300

    Módulo Intermediário · Capítulo 08 · Vídeo 04

---
+++ capitulo-08/example_08_04.py
@@ -0,0 +1,18 @@
+# ---------------------------------------------------------------------------------------------------------------------
+# ARQUIVO: example_08_04.py
+# DATA DE CRIAÇÃO: 30-05-2025
+# DATA DE PUBLICAÇÃO: 30-05-2025
+# CONTEUDISTA: Faculdade de Tecnologia (FATEC) do Estado de São Paulo
+# CONTEÚDO OFERTADO: Curso de Python – Módulo Intermediário
+# DOCENTE AUTOR: Sérgio Luiz Banin
+# DISCENTE: Thiago Bernegossi
+# DESCRIÇÃO: Arquivo que contém código-fonte desenvolvido na linguagem de programação Python para fins de aprendizado.
+# ---------------------------------------------------------------------------------------------------------------------
+
+print('A execução do sistema informático foi iniciada.\n')
+
+number = int(input('Digite um número inteiro: '))
+value = 'par' if number % 2 == 0 else 'ímpar'
+print(f'O número inteiro {number} é {value}!')
+
+print('\nA execução do sistema informático foi concluída.')
