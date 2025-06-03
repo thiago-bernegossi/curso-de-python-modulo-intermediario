b7b43be15a3ad855d986bd496b9c1505753267a6
Thiago Bernegossi <thiago.bernegossi@outlook.com.br>
2025-06-03 17:53:13 -0300

    Módulo Intermediário · Capítulo 09 · Vídeo 04

---
+++ capitulo-09/example_09_04.py
@@ -0,0 +1,23 @@
+# ---------------------------------------------------------------------------------------------------------------------
+# ARQUIVO: example_09_04.py
+# DATA DE CRIAÇÃO: 03-06-2025
+# DATA DE PUBLICAÇÃO: 03-06-2025
+# CONTEUDISTA: Faculdade de Tecnologia (FATEC) do Estado de São Paulo
+# CONTEÚDO OFERTADO: Curso de Python – Módulo Intermediário
+# DOCENTE AUTOR: Sérgio Luiz Banin
+# DISCENTE: Thiago Bernegossi
+# DESCRIÇÃO: Arquivo que contém código-fonte desenvolvido na linguagem de programação Python para fins de aprendizado.
+# ---------------------------------------------------------------------------------------------------------------------
+
+print('A execução do sistema informático foi iniciada.\n')
+
+number_01 = 26
+print(f'Utilizando a função hash na variável number_01 e exibindo o resultado: {hash(number_01)}\n')
+
+number_02 = 26.00000
+print(f'Utilizando a função hash na variável number_02 e exibindo o resultado: {hash(number_02)}\n')
+
+number_03 = 26.00001
+print(f'Utilizando a função hash na variável number_03 e exibindo o resultado: {hash(number_03)}')
+
+print('\nA execução do sistema informático foi concluída.')
