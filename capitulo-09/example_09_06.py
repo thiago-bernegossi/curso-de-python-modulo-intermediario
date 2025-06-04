e667724c3fef946218f5c21c03dc5346474b7a21
Thiago Bernegossi <thiago.bernegossi@outlook.com.br>
2025-06-04 17:54:27 -0300

    Módulo Intermediário · Capítulo 09 · Vídeo 06

---
+++ capitulo-09/example_09_06.py
@@ -0,0 +1,24 @@
+# ---------------------------------------------------------------------------------------------------------------------
+# ARQUIVO: example_09_06.py
+# DATA DE CRIAÇÃO: 04-06-2025
+# DATA DE PUBLICAÇÃO: 04-06-2025
+# CONTEUDISTA: Faculdade de Tecnologia (FATEC) do Estado de São Paulo
+# CONTEÚDO OFERTADO: Curso de Python – Módulo Intermediário
+# DOCENTE AUTOR: Sérgio Luiz Banin
+# DISCENTE: Thiago Bernegossi
+# DESCRIÇÃO: Arquivo que contém código-fonte desenvolvido na linguagem de programação Python para fins de aprendizado.
+# ---------------------------------------------------------------------------------------------------------------------
+
+print('A execução do sistema informático foi iniciada.\n')
+
+print('Primeira iteração do laço de repetição for:')
+set_01 = {3.3, 18.6, 34.0, 43.1, 58.7}
+for counter in set_01:
+    print(counter)
+
+print(f'\nSegunda iteração do laço de repetição for:')
+set_01 = {18.6, 3.3, 58.7, 34.0, 43.1,}
+for counter in set_01:
+    print(counter)
+
+print('\nA execução do sistema informático foi concluída.')
