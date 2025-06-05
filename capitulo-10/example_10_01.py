17c9110331853baf761e8c86d6ddb63879b86034
Thiago Bernegossi <thiago.bernegossi@outlook.com.br>
2025-06-05 17:34:01 -0300

    Módulo Intermediário · Capítulo 10 · Vídeo 01

---
+++ capitulo-10/example_10_01.py
@@ -0,0 +1,19 @@
+# ---------------------------------------------------------------------------------------------------------------------
+# ARQUIVO: example_10_01.py
+# DATA DE CRIAÇÃO: 05-06-2025
+# DATA DE PUBLICAÇÃO: 05-06-2025
+# CONTEUDISTA: Faculdade de Tecnologia (FATEC) do Estado de São Paulo
+# CONTEÚDO OFERTADO: Curso de Python – Módulo Intermediário
+# DOCENTE AUTOR: Sérgio Luiz Banin
+# DISCENTE: Thiago Bernegossi
+# DESCRIÇÃO: Arquivo que contém código-fonte desenvolvido na linguagem de programação Python para fins de aprendizado.
+# ---------------------------------------------------------------------------------------------------------------------
+
+print('A execução do sistema informático foi iniciada.\n')
+
+dictionary_01 = {'ES': 'Espírito Santo', 'MG': 'Minas Gerais', 'RJ': 'Rio de Janeiro', 'SP': 'São Paulo'}
+print(f'Exibindo o número de elementos do dicionário 01: {len(dictionary_01)}')
+print(f'Exibindo os elementos do dicionário 01: {dictionary_01}')
+print(f'Exibindo o tipo de objeto do dicionário 01: {type(dictionary_01)}')
+
+print('\nA execução do sistema informático foi concluída.')
