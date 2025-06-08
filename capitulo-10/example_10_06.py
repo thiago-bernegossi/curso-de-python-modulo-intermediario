9c84f02d0a97455de4c81f740bdf90eb2362503c
Thiago Bernegossi <thiago.bernegossi@outlook.com.br>
2025-06-07 23:55:25 -0300

    Módulo Intermediário · Capítulo 10 · Vídeo 06

---
+++ capitulo-10/example_10_06.py
@@ -0,0 +1,19 @@
+# ---------------------------------------------------------------------------------------------------------------------
+# ARQUIVO: example_10_06.py
+# DATA DE CRIAÇÃO: 07-06-2025
+# DATA DE PUBLICAÇÃO: 07-06-2025
+# CONTEUDISTA: Faculdade de Tecnologia (FATEC) do Estado de São Paulo
+# CONTEÚDO OFERTADO: Curso de Python – Módulo Intermediário
+# DOCENTE AUTOR: Sérgio Luiz Banin
+# DISCENTE: Thiago Bernegossi
+# DESCRIÇÃO: Arquivo que contém código-fonte desenvolvido na linguagem de programação Python para fins de aprendizado.
+# ---------------------------------------------------------------------------------------------------------------------
+
+print('A execução do sistema informático foi iniciada.\n')
+
+print('Caso 02-) Exibição do dicionário:')
+dictionary_01 = {'01': 'Amarelo', '02': 'Azul', '03': 'Verde', '04': 'Vermelho'}
+for counter in dictionary_01.values():
+    print(f'{counter}')
+
+print('\nA execução do sistema informático foi concluída.')
