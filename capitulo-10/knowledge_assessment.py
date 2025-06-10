f2231acee0edd0e0bdfb931ecb7dc568ec9da314
Thiago Bernegossi <thiago.bernegossi@outlook.com.br>
2025-06-10 00:58:28 -0300

    Módulo Intermediário · Capítulo 10 · Avaliação de Conhecimentos

---
+++ capitulo-10/knowledge_assessment.py
@@ -0,0 +1,25 @@
+# ---------------------------------------------------------------------------------------------------------------------
+# ARQUIVO: knowledge_assessment.py
+# DATA DE CRIAÇÃO: 10-06-2025
+# DATA DE PUBLICAÇÃO: 10-06-2025
+# CONTEUDISTA: Faculdade de Tecnologia (FATEC) do Estado de São Paulo
+# CONTEÚDO OFERTADO: Curso de Python – Módulo Intermediário
+# DOCENTE AUTOR: Sérgio Luiz Banin
+# DISCENTE: Thiago Bernegossi
+# DESCRIÇÃO: Arquivo que contém código-fonte desenvolvido na linguagem de programação Python para fins de aprendizado.
+# ---------------------------------------------------------------------------------------------------------------------
+
+# Questão 01-) Dado o dicionário que segue:
+ssolar = { 'Mercúrio': 4879.4,
+           'Vênus': 12104,
+           'Terra': 12742,
+           'Marte': 6779,
+           'Júpiter': 139829,
+           'Saturno': 116460,
+           'Urano': 50724
+}
+# Que operações precisamos para: recuperar o valor que corresponde à 'Marte', obter uma lista com apenas os valores contidos no dicionário, incluir o planeta 'Netuno' com diâmetro 49244?
+# Resposta: b. ssolar['Marte'], ssolar.values(), ssolar['Netuno'] = 49244
+
+# Questão 02-) A linguagem de programação Python dispõe de várias estruturas de dados nativas, além de uma semântica que facilita sua criação. Como podem ser criadas: uma lista com os elementos 1 e 2; uma tupla com o elemento 'um'; um conjunto vazio; e um dicionário vazio?')
+# Resposta: d. [ 1, 2 ], ( 'um', ), set(), { }
