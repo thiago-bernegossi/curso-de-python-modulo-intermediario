9bf248096521d7945dd8d058974a4149f70617fc
Thiago Bernegossi <thiago.bernegossi@outlook.com.br>
2025-06-11 21:21:33 -0300

    Módulo Intermediário · Capítulo 11 · Avaliação de Conhecimentos

---
+++ capitulo-11/knowledge_assessment.py
@@ -0,0 +1,22 @@
+# ---------------------------------------------------------------------------------------------------------------------
+# ARQUIVO: knowledge_assessment.py
+# DATA DE CRIAÇÃO: 11-06-2025
+# DATA DE PUBLICAÇÃO: 11-06-2025
+# CONTEUDISTA: Faculdade de Tecnologia (FATEC) do Estado de São Paulo
+# CONTEÚDO OFERTADO: Curso de Python – Módulo Intermediário
+# DOCENTE AUTOR: Sérgio Luiz Banin
+# DISCENTE: Thiago Bernegossi
+# DESCRIÇÃO: Arquivo que contém código-fonte desenvolvido na linguagem de programação Python para fins de aprendizado.
+# ---------------------------------------------------------------------------------------------------------------------
+
+# Questão 01-) Os arquivo são um importante recurso de programação, pois permitem salvar dados de qualquer natureza, possibilitando sua recuperação posterior, evitando que sejam pedidos, mesmo com o dispositivo desligado. Ainda sobre os arquivos, podemos afirmar que:
+# I. existem duas categorias essenciais de arquivos: texto e binários.
+# II. nos arquivo texto, cada byte é tratado como um caractere legível, que pode ser apresentado ao usuário sem necessidade de interpretação, desde que utilizada a codificação correta do arquivo.
+# III. nos arquivos binários, os bytes precisam ser interpretados conforme a estrutura prevista para seu tipo específico de conteúdo.
+# Resposta: d. todas as afirmativas estão corretas.
+
+# Questão 02-) Um arquivo é:
+# I. um recurso de armazenamento de dados disponíveis em todos os tipos de dispositivos digitais.
+# II. as operações de acesso, gravação e leitura dos arquivos são de responsabilidade exclusiva da linguagem de programação.
+# III. o conteúdo de todo arquivo é uma coleção de bytes, organizados de maneira apropriada conforme seu tipo de conteúdo.
+# Resposta: c. apenas as afirmativas I e III estão corretas.
