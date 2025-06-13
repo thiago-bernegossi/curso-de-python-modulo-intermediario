f65f8abc31844e4803ea660cba4c778eadc6e237
Thiago Bernegossi <thiago.bernegossi@outlook.com.br>
2025-06-13 20:39:19 -0300

    Módulo Intermediário · Capítulo 12 · Vídeo 06

---
+++ capitulo-12/example_12_06.py
@@ -0,0 +1,20 @@
+# ---------------------------------------------------------------------------------------------------------------------
+# ARQUIVO: example_12_06.py
+# DATA DE CRIAÇÃO: 13-06-2025
+# DATA DE PUBLICAÇÃO: 13-06-2025
+# CONTEUDISTA: Faculdade de Tecnologia (FATEC) do Estado de São Paulo
+# CONTEÚDO OFERTADO: Curso de Python – Módulo Básico
+# DOCENTE AUTOR: Sérgio Luiz Banin
+# DISCENTE: Thiago Bernegossi
+# DESCRIÇÃO: Arquivo que contém código-fonte desenvolvido na linguagem de programação Python para fins de aprendizado.
+# ---------------------------------------------------------------------------------------------------------------------
+
+print('A execução do sistema informático foi iniciada.\n')
+
+def greeting(name, message='Olá,'):
+    print(message, name)
+
+greeting('Mundo!', 'Bom dia,')
+greeting('Mundo!')
+
+print(f'\nA execução do sistema informático foi concluída.')
