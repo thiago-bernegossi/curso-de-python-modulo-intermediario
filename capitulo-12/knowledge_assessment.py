13ef3e72f26296491452c4ef4aa3beac09b2cf2a
Thiago Bernegossi <thiago.bernegossi@outlook.com.br>
2025-06-17 16:23:37 -0300

    Módulo Intermediário · Capítulo 12 · Avaliação de Conhecimentos

---
+++ capitulo-12/knowledge_assessment.py
@@ -0,0 +1,16 @@
+# ---------------------------------------------------------------------------------------------------------------------
+# ARQUIVO: knowledge_assessment.py
+# DATA DE CRIAÇÃO: 17-06-2025
+# DATA DE PUBLICAÇÃO: 17-06-2025
+# CONTEUDISTA: Faculdade de Tecnologia (FATEC) do Estado de São Paulo
+# CONTEÚDO OFERTADO: Curso de Python – Módulo Intermediário
+# DOCENTE AUTOR: Sérgio Luiz Banin
+# DISCENTE: Thiago Bernegossi
+# DESCRIÇÃO: Arquivo que contém código-fonte desenvolvido na linguagem de programação Python para fins de aprendizado.
+# ---------------------------------------------------------------------------------------------------------------------
+
+# Questão 01-) A definição de funções Python pode, a critério do programador, atribuir valores padrão para um ou mais de seus parâmetros, de maneira que sejam como parâmetros default. Tais parâmetros default não precisam ser indicados na chamada da função, simplificando seu uso, mas podem ser fornecidos quando desejado, para que outros valores sejam utilizados. Esta prática flexibiliza o uso de funções. Verifique a seguir, qual declaração de função indica corretamente parâmetros default.
+# Resposta: d. def f_cps(en, pa = True, so = 20): pass
+
+# Questão 02-) Considere a definição e uso de funções durante o projeto e desenvolvimento de um programa Python.
+# Resposta: a. O entendimento de pequenos trechos de código é facilitado, assim o uso de funções torna um programa mais fácil de ser entendido, pois sua organização se torna mais simples. Além disso, o tamanho do programa diminui conforme as funções reduzem a repetição de código.
