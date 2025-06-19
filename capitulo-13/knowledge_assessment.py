d22ad1a72700cea7b122d40d9232678c8731b96f
Thiago Bernegossi <thiago.bernegossi@outlook.com.br>
2025-06-18 22:02:16 -0300

    Módulo Intermediário · Capítulo 13 · Avaliação de Conhecimentos

---
+++ capitulo-13/knowledge_assessment.py
@@ -0,0 +1,17 @@
+# ---------------------------------------------------------------------------------------------------------------------
+# ARQUIVO: knowledge_assessment.py
+# DATA DE CRIAÇÃO: 18-06-2025
+# DATA DE PUBLICAÇÃO: 18-06-2025
+# CONTEUDISTA: Faculdade de Tecnologia (FATEC) do Estado de São Paulo
+# CONTEÚDO OFERTADO: Curso de Python – Módulo Intermediário
+# DOCENTE AUTOR: Sérgio Luiz Banin
+# DISCENTE: Thiago Bernegossi
+# DESCRIÇÃO: Arquivo que contém código-fonte desenvolvido na linguagem de programação Python para fins de aprendizado.
+# ---------------------------------------------------------------------------------------------------------------------
+
+# Questão 01-) Qual o efeito de utilizar o list comprehension sem atribuir seu resultado a uma variável?
+# Resposta: d. A lista é criada e descartada.
+
+# Questão 02-) Qual o resultado armazenado em R após a execução de:
+R = [ a * 2 for a in ['1', '2', '3']]
+# Resposta: c. ['11', '22', '33']
